3e8584a4208b1b47d3be7b09f45099c8ed2ee43b
ekozina <katty6991@gmail.com>
2025-07-31 23:27:21 +0200

    добавлена функция генератор рандомной строки

--- tools/fakers.py
+++ tools/fakers.py
@@ -1,5 +1,11 @@
+import random
 import time
+import string
 
 
 def get_random_email() -> str:
     return f"test.{time.time()}@example.com"
+
+def generate_random_string(length: int) -> str:
+    value = [random.choice(string.ascii_lowercase) for _ in range(length)]
+    return "".join(value)
