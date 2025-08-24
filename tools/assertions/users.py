81eda66cfbfb0aef69c4c45b7a56e6aebfd92a6d
ekozina <katty6991@gmail.com>
2025-08-24 20:12:53 +0200

    Фикстуры Pytest. Практика написания автотестов с использованием фикстур

--- tools/assertions/users.py
+++ tools/assertions/users.py
@@ -1,4 +1,5 @@
-from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema
+from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema, UserSchema, \
+    GetUserResponseSchema
 from tools.assertions.base import assert_equal
 
 
@@ -14,3 +15,27 @@ def assert_create_user_response(request: CreateUserRequestSchema, response: Crea
     assert_equal(response.user.last_name, request.last_name, "last_name")
     assert_equal(response.user.first_name, request.first_name, "first_name")
     assert_equal(response.user.middle_name, request.middle_name, "middle_name")
+
+def assert_user(expected: UserSchema, actual: UserSchema):
+    """
+    Проверяет, что полученные данные о пользователе соответствуют ожидаемым.
+
+    :param expected: Ожидаемые данные пользователя.
+    :param actual: Ответ API с данными пользователя.
+    :raises AssertionError: Если хотя бы одно поле не совпадает.
+    """
+    assert_equal(actual.id, expected.id, "id")
+    assert_equal(actual.email, expected.email, "email")
+    assert_equal(actual.first_name, expected.first_name, "first_name")
+    assert_equal(actual.last_name, expected.last_name, "last_name")
+    assert_equal(actual.middle_name, expected.middle_name, "middle_name")
+
+def assert_get_user_response(create_user_response: UserSchema, get_user_response: UserSchema, ):
+    """
+    Проверяет, что полученные данные о пользователе соответствуют полученным данным при создании пользователя.
+
+    :param create_user_response: Данные пользователя, полученные при создании.
+    :param get_user_response: Ответ API с данными пользователя.
+    :raises AssertionError: Если хотя бы одно поле не совпадает.
+    """
+    assert_user(create_user_response, get_user_response)
