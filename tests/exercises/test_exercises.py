bc178a866e3f649fd68acb196cc6ea3f4c4eb4ae
ekozina <katty6991@gmail.com>
2025-09-16 12:13:39 +0200

    Интеграция Allure в автотесты. Практическая работа с Allure аннотациями

--- tests/exercises/test_exercises.py
+++ tests/exercises/test_exercises.py
@@ -37,10 +37,13 @@ from tools.assertions.schema import validate_json_schema
 @allure.tag(AllureTag.EXERCISES, AllureTag.REGRESSION)
 @allure.epic(AllureEpic.LMS)
 @allure.feature(AllureFeature.EXERCISES)
+@allure.parent_suite(AllureEpic.LMS)
+@allure.suite(AllureFeature.EXERCISES)
 class TestExercises:
 
     @allure.tag(AllureTag.CREATE_ENTITY)
     @allure.story(AllureStory.CREATE_ENTITY)
+    @allure.sub_suite(AllureStory.CREATE_ENTITY)
     @allure.severity(Severity.BLOCKER)
     @allure.title("Create exercise")
     def test_create_exercise(self, exercises_client: ExercisesClient, function_course: CourseFixture):
@@ -55,6 +58,7 @@ class TestExercises:
 
     @allure.story(AllureStory.GET_ENTITY)
     @allure.tag(AllureTag.GET_ENTITY)
+    @allure.sub_suite(AllureStory.GET_ENTITY)
     @allure.severity(Severity.BLOCKER)
     @allure.title("Get exercise")
     def test_get_exercise(self, exercises_client: ExercisesClient, function_exercise: ExerciseFixture):
@@ -68,6 +72,7 @@ class TestExercises:
 
     @allure.story(AllureStory.UPDATE_ENTITY)
     @allure.tag(AllureTag.UPDATE_ENTITY)
+    @allure.sub_suite(AllureStory.UPDATE_ENTITY)
     @allure.severity(Severity.CRITICAL)
     @allure.title("Update exercise")
     def test_update_exercise(self, exercises_client: ExercisesClient, function_exercise: ExerciseFixture):
@@ -82,6 +87,7 @@ class TestExercises:
 
     @allure.story(AllureStory.DELETE_ENTITY)
     @allure.tag(AllureTag.DELETE_ENTITY)
+    @allure.sub_suite(AllureStory.DELETE_ENTITY)
     @allure.severity(Severity.CRITICAL)
     @allure.title("Delete exercise")
     def test_delete_exercise(self, exercises_client: ExercisesClient, function_exercise: ExerciseFixture):
@@ -98,6 +104,7 @@ class TestExercises:
 
     @allure.story(AllureStory.GET_ENTITIES)
     @allure.tag(AllureTag.GET_ENTITIES)
+    @allure.sub_suite(AllureStory.GET_ENTITIES)
     @allure.severity(Severity.BLOCKER)
     @allure.title("Get exercises")
     def test_get_exercises(self, exercises_client: ExercisesClient, function_exercise: ExerciseFixture):
