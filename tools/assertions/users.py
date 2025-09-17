fae610761139b2acda9fe5f9f98d0123f0f249ab
ekozina <katty6991@gmail.com>
2025-09-17 12:49:54 +0200

    Добавление Allure шагов в автотесты. Расстановка allure.step для проверок

--- tools/assertions/users.py
+++ tools/assertions/users.py
@@ -1,8 +1,11 @@
+import allure
+
 from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema, UserSchema, \
     GetUserResponseSchema
 from tools.assertions.base import assert_equal
 
 
+@allure.step("Check create user response")
 def assert_create_user_response(request: CreateUserRequestSchema, response: CreateUserResponseSchema):
     """
     Проверяет, что ответ на создание пользователя соответствует запросу.
@@ -16,6 +19,8 @@ def assert_create_user_response(request: CreateUserRequestSchema, response: Crea
     assert_equal(response.user.first_name, request.first_name, "first_name")
     assert_equal(response.user.middle_name, request.middle_name, "middle_name")
 
+
+@allure.step("Check user")
 def assert_user(expected: UserSchema, actual: UserSchema):
     """
     Проверяет, что полученные данные о пользователе соответствуют ожидаемым.
@@ -30,6 +35,8 @@ def assert_user(expected: UserSchema, actual: UserSchema):
     assert_equal(actual.last_name, expected.last_name, "last_name")
     assert_equal(actual.middle_name, expected.middle_name, "middle_name")
 
+
+@allure.step("Check get user response")
 def assert_get_user_response(create_user_response: UserSchema, get_user_response: UserSchema, ):
     """
     Проверяет, что полученные данные о пользователе соответствуют полученным данным при создании пользователя.
